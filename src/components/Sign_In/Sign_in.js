50b409d24455ef49ae3869ea790b10f87add3e77
Myroslav Derevianko <derevianko_miroslav@ukr.net>
2019-03-25 16:44:14 +0200

    Add close button to global wrap

--- src/components/Sign_In/Sign_in.js
+++ src/components/Sign_In/Sign_in.js
@@ -8,7 +8,6 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loginUserBySocial } from '../../actions/authaction'
 
-
 class SignIn extends Component {
 
   
--- src/components/common/GlobalWrap.js
+++ src/components/common/GlobalWrap.js
@@ -2,18 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./GlobalWrap.css";
 
-const GlobalWrap = ({ children, styles }) => {
+const GlobalWrap = ({ children, styles, close }) => {
     return (
         <div className={`container col ai-stretch jc-stretch globalWrapContainer ${(styles) ? styles : ''}`}>
             <div className="globalWrapContent">
+                { close && <CloseBtn click={close}/>}
                 {children}
             </div>
         </div>
     )
 }
 
+const CloseBtn = ({ click }) => {
+    return (
+        <div className="gw-close" onClick={click}>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
+
 GlobalWrap.propTypes = {
    styles: PropTypes.string,
+   close: PropTypes.func,
 }
 
 export default GlobalWrap;
